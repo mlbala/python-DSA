e635fcbf36ffda7f7f2536b932c827fa0f9bb0d7
Balakrishnan <bk@Balakrishnans-MacBook-Air.local>
2024-10-15 19:02:36 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,10 @@
+
+import tensorflow as tf
+
+# Force TensorFlow to use the GPU
+with tf.device('/GPU:0'):
+    a = tf.random.normal([1000, 1000])
+    b = tf.random.normal([1000, 1000])
+    c = tf.matmul(a, b)
+
+print("GPU operation successful!")
